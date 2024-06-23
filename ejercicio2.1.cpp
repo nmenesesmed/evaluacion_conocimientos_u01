ad4892746ea3d7ba5f2d9f48cd35df73198a8038
nmenesesmed <nmenesesmed@unjbg.edu.pe>
2024-06-23 15:36:25 -0500

    ejercicio2.1

---
+++ ejercicio2.1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n1, n2;
+    cout << "Ingresa un primer numero: "; 
+    cin >> n1;
+    cout << "Ingresa un segundo numero: "; 
+    cin >> n2;
+
+    int C = 0;
+    int i = 1;
+
+    while (i <= n1 && i <= n2) {
+        if (n1 % i == 0 && n2 % i == 0) {
+            C = C + 1;
+        }
+        i = i + 1;
+    }
+
+    if (C > 1) {
+        cout << "NO son PESI" << endl;
+    } else {
+        cout << "Son PESI" << endl;
+    }
+
+    return 0;
+}
